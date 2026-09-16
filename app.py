51b8b834da9f50dba49b6752b6ed16dd7c0fcd9b
saransh2396 <saranshoffice@gmail.com>
2026-09-16 18:25:28 +0530

    Update API message

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ class CustomerData(BaseModel):
 
 @app.get("/")
 def home():
-    return {"message": "Churn prediction API is running"}
+    return  {"message": "Churn prediction API is running - v2"}
 
 
 @app.post("/predict")
